efa74291226fa439b3eea604c6b0de5757af0e4b
Arafat-Hossain-Anik <arafat15@cse.pstu.ac.bd>
2022-06-26 03:45:01 +0600

    pagination added

--- src/components/Contest/Contest.js
+++ src/components/Contest/Contest.js
@@ -2,24 +2,27 @@ import React from 'react';
 import Countdown from 'react-countdown';
 
 
-const Completionist = () => <span>You are good to go!</span>;
+const Completionist = () => <span>Contest Is Over</span>;
 
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
         return <Completionist />;
     } else {
-        return <span style={{ fontSize: "40px", fontWeight: "bold" }}>{days}:{hours}:{minutes}:{seconds}</span>;
+        return (
+            <div>
+                <span style={{ fontSize: "40px", fontWeight: "bold" }}>{days}:{hours}:{minutes}:{seconds}</span> <br />
+                <button className='btn btn-success'>Enter Now</button>
+            </div>);
     }
 };
 const Contest = (props) => {
     const { contest } = props;
-    console.log(contest);
     return (
 
         <div className="col-sm-6">
             <div className="card mb-3">
-                <img src="https://rstheme.com/products/html/shooter/shooter-html/images/about/3.jpg" className="card-img-top" alt="..." />
+                <img src="https://rstheme.com/products/html/shooter/shooter-html/images/about/3.jpg" className="card-img-top" alt="..." style={{ height: '300px' }} />
                 <div className="card-body">
                     <h5 className="card-title">{contest.name}</h5>
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
--- src/components/Contest/Contests.js
+++ src/components/Contest/Contests.js
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import Contest from './Contest';
+import './Contest.css';
 
 const Contests = () => {
     const [contests, setContests] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0);
+    const size = 2;
     useEffect(() => {
-        fetch('http://localhost:3010/contest')
+        fetch(`http://localhost:3010/contests?page=${page}&&size=${size}`)
             .then(res => res.json())
-            .then(res => setContests(res));
-    }, []);
-    // console.log(contests.map(ct => ct.dateTime));
+            .then(data => {
+                setContests(data.result);
+                const count = data.count;
+                const pageNumber = Math.ceil(count / size);
+                setPageCount(pageNumber);
+            });
+    }, [page]);
     return (
         <div className='container mt-3'>
             <div className='d-flex align-items-center flex-column'>
                 <h2>OUR RUNNING <span style={{ color: "#a00b0b" }}>CONTEST</span></h2>
                 <hr style={{ width: "10%", margin: "0", borderTop: "3px solid #a00b0b" }} />
             </div>
-            <div className="row mt-4 justify-content-center">
+            {/* pagination */}
+            <div className="btn-group paginator" role="group" aria-label="Basic outlined example">
+                {
+                    [...Array(pageCount).keys()].map(number => <button className={number === page ? 'btn btn-outline-primary active' : 'btn btn-outline-primaty'} key={number} onClick={() => setPage(number)}>{number + 1}</button>)
+                }
+            </div>
+            <div className="row mt-4">
                 {
                     contests.map(contest => <Contest key={contest._id} contest={contest}></Contest>)
                 }
